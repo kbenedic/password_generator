a55d24769df60184c7c90079eaff421a6071b6e2
kbenedic <kbenedic@outlook.fr>
2025-02-15 11:05:06 +0100

    Ajout du générateur de mot de passe sécurisé

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Bienvenue dans le générateur de mots de passe sécurisé !")
+import random
+import string
+
+#fonction pour générer un mot de passe sécurisé
+def generate_password(length=12):
+    # Définir les caractères possible
+    characters = string.ascii_letters + string.digits +string.punctuation
+    #Générer le mot de passe aléatoirement
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+# Test de génération d'un mot de passe
+if __name__== "__main__":
+    password = generate_password(16) # Générer un mdp en 16 carac.
+    print(f"mot de passe généré : {password}")
